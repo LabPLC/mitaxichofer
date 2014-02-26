a216e18fe8d355c8f64789864fcf0ca343742d29
mikesaurio <mikesaurio@gmail.com>
2014-02-26 12:55:50 -0600

    clean NodeJs

--- Mitaxichofer/src/codigo/labplc/mx/mitaxichofer/tracking/SocketConnection.java
+++ Mitaxichofer/src/codigo/labplc/mx/mitaxichofer/tracking/SocketConnection.java
@@ -93,7 +93,7 @@ public class SocketConnection {
 			@Override
 			public void on(String event, IOAcknowledge ack, Object... args) {
 			//	System.out.println("Servidor de eventos activa '" + event + "'");
-				System.out.println("Servidor de eventos activa '" + args[0] + "'");
+			//	System.out.println("Servidor de eventos activa '" + args[0] + "'");
 				try {
 					  JSONObject jsonObj = new JSONObject(args[0].toString());
 		                String disposicion = jsonObj.getString("disposicion");
